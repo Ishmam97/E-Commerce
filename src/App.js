2b0515edeaa3ca4fb3ad0dd113a01d569a81307b
Ishmam97 <43916219+Ishmam97@users.noreply.github.com>
2021-01-08 04:30:10 +0600

    initial commit
    ecommerce

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Card from './Components/Card'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello from app</h1>
+      <Card/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Card() {
+    return (
+      
+      <div>
+        <h3>Hello from Card</h3>
+      </div>
+      
+    );
+  }
+  
+  export default Card;
+  
