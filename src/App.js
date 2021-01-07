28ce32fc38b76151ee6af8c528144190952f454a
Ishmam97 <43916219+Ishmam97@users.noreply.github.com>
2021-01-08 04:48:50 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Card from './Components/Card'
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h1>Hello from app</h1>
       <Card/>
     </div>
---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+      
+      <div>
+        <h3>Hello from Navbar</h3>
+      </div>
+      
+    );
+  }
+
+  export default Navbar;
