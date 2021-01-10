b8801c347accf952dbb9b9a7a54f881d20af9719
Ishmam97 <43916219+Ishmam97@users.noreply.github.com>
2021-01-10 06:01:28 +0600

    updoot

--- frontend/src/Components/Signin.js
+++ frontend/src/Components/Signin.js
@@ -1,9 +1,29 @@
 import react from 'react';
+import {Link} from 'react-router-dom'
+import './css/signin.css'
+
 
 const Signin = ()=>{
     
     return(
-        <h1>Hello from Signin</h1>
+        <section className="section">
+            <div className="wrapper">
+                <div className="user singinBx">
+                    <div className="imgBx">
+                        <img src="https://picsum.photos/600/600" alt="random pic"/>                    
+                    </div>
+                    <div className="formBx">
+                        <form>
+                            <h2>Sign In</h2>
+                            <input type="text" name="uname" placeholder="Username"/>
+                            <input type="password" name="pass" placeholder="password"/>
+                            <input type="submit" value="Login"/>
+                            <p className="signup">Don't have an account? <Link to='#'>Sign Up.</Link></p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
