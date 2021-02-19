3bf6a960b8d54893e7365e9ba98ef2bdb693f073
Ishmam97 <43916219+Ishmam97@users.noreply.github.com>
2021-02-19 23:55:39 +0600

    t

--- frontend/src/Components/Card.js
+++ frontend/src/Components/Card.js
@@ -1,16 +1,18 @@
 import React from "react";
-
+import './css/card.css'
 
 function Card({id , img , name , price , qt , cat, desc}) {
   return (
     <div className="col-md-4 my-3">
-      <div className='card h-100'>
-        <h1>{name}</h1>
-        <a href="#">
-          <img className='img-fluid' src={`http://localhost:9001/${img}`} alt="product"/>
-        </a>        
-        <p className="text-justify">{desc}</p>
-        <span>{price}</span>
+      <div className='card'>
+        <h1 className='text-center'>{name}</h1>
+        <div className='text-center'>
+          <a href="#">
+            <img className='img-fluid pImg' src={`http://localhost:9001/uploads/${img}`} alt="product"/>
+          </a> 
+        </div>       
+        <p className="text-justify text-center">{desc}</p>
+        <span className="text-center">Tk.{price}</span>
       </div>  
     </div>
     
--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json())
 
 app.use(cookieParser())
 // app.use(express.static('public'));
-app.use(express.static('uploads'))
+app.use('/uploads' , express.static('uploads'))
 app.use('/api/images' , require('./routes/images'))
 
 //connect to mongoDB
