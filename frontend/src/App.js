7979df39d92c57d10354f6ef776aa6409b395a74
Ishmam Solaiman <ishmam.a.solaiman@gmail.com>
2022-12-17 02:14:09 -0600

    update outdated routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,51 @@
 import './App.css';
 import React, { useEffect } from 'react';
 
-// import Navigation from './Components/Navigation';
-// import Home from './Components/Home'
-// import Signin from './Components/Signin'
-// import NotFound from './Components/NotFound'
-// import UserDash from './Components/UserDash'
-// import AdminDash from './Components/AdminDash'
-// import AdminEditProduct from './Components/AdminEditProduct'
-// import AdminRoute from './Components/AdminRoute'
-// import UserRoute from './Components/UserRoute'
+import Navigation from './Components/Navigation';
+import Home from './Components/Home'
+import Signin from './Components/Signin'
+import NotFound from './Components/NotFound'
+import UserDash from './Components/UserDash'
+import AdminDash from './Components/AdminDash'
+import AdminEditProduct from './Components/AdminEditProduct'
+import AdminRoute from './Components/AdminRoute'
+import UserRoute from './Components/UserRoute'
 
-import {BrowserRouter , Switch , Route} from 'react-router-dom'
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
 //redux
-// import {getProducts} from './Redux/actions/productActions'
+import {getProducts} from './Redux/actions/productActions'
 import {useDispatch} from 'react-redux'
 
 const App = ()=>{ 
   
-  // const dispatch = useDispatch();
-  // useEffect(()=>{
-  //   dispatch(getProducts())
-  // },[dispatch])
-
-  // return (
-  //   <BrowserRouter>
-  //     {/*render navbar*/}
-  //     <Navigation/>
-  //     <main>
-  //       <Switch>
-  //         {/*react routing*/}
-  //         <Route exact path='/' component={Home}/>
-  //         <Route exact path='/signin' component={Signin}/>
-          
-  //         <UserRoute exact path='/user/dash' component={UserDash}/>
-  //         <AdminRoute exact path='/admin/dash' component={AdminDash}/>
-  //         <AdminRoute exact path='/admin/edit/product/:pId' component={AdminEditProduct}/>
-  //         <Route component={NotFound}/>
-  //       </Switch>
-  //     </main>
-  //   </BrowserRouter>
-  // ); 
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(getProducts())
+  },[dispatch])
 
   return (
-            <h1>Hello</h1>
-    );
+    <BrowserRouter>
+      {/*render navbar*/}
+      <Navigation/>
+      <main>
+        <Routes>
+          {/*react routing*/}
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/signin' element={<Signin/>}/>
+          <Route exact path='/user/dash' element={<UserRoute/>}>
+            <Route exact path='/user/dash' element={<UserDash/>}/>
+          </Route>
+          <Route exact path='/admin/dash' element={<AdminRoute/>}>
+            <Route exact path='/admin/dash' element={<AdminDash/>}/>
+          </Route>
+          <Route exact path='/admin/edit/product/:pId' element={<AdminRoute/>}>
+            <Route exact path='/admin/edit/product/:pId' element={<AdminEditProduct/>}/>
+          </Route>
+          <Route component={NotFound}/>
+        </Routes>
+      </main>
+    </BrowserRouter>
+  ); 
 }  
 
 
