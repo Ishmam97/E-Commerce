3cd3d045f1732ef80af60e09e28b2209964972f5
Ishmam Solaiman <ishmam.a.solaiman@gmail.com>
2022-12-17 01:06:08 -0600

    hello new world

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
 
-function App() {
+// import Navigation from './Components/Navigation';
+// import Home from './Components/Home'
+// import Signin from './Components/Signin'
+// import NotFound from './Components/NotFound'
+// import UserDash from './Components/UserDash'
+// import AdminDash from './Components/AdminDash'
+// import AdminEditProduct from './Components/AdminEditProduct'
+// import AdminRoute from './Components/AdminRoute'
+// import UserRoute from './Components/UserRoute'
+
+import {BrowserRouter , Switch , Route} from 'react-router-dom'
+//redux
+// import {getProducts} from './Redux/actions/productActions'
+import {useDispatch} from 'react-redux'
+
+const App = ()=>{ 
+  
+  // const dispatch = useDispatch();
+  // useEffect(()=>{
+  //   dispatch(getProducts())
+  // },[dispatch])
+
+  // return (
+  //   <BrowserRouter>
+  //     {/*render navbar*/}
+  //     <Navigation/>
+  //     <main>
+  //       <Switch>
+  //         {/*react routing*/}
+  //         <Route exact path='/' component={Home}/>
+  //         <Route exact path='/signin' component={Signin}/>
+          
+  //         <UserRoute exact path='/user/dash' component={UserDash}/>
+  //         <AdminRoute exact path='/admin/dash' component={AdminDash}/>
+  //         <AdminRoute exact path='/admin/edit/product/:pId' component={AdminEditProduct}/>
+  //         <Route component={NotFound}/>
+  //       </Switch>
+  //     </main>
+  //   </BrowserRouter>
+  // ); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+            <h1>Hello</h1>
+    );
+}  
+
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+// import store from "./Redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  // <Provider store={store}>    
     <App />
-  </React.StrictMode>
+  // </Provider>,
+  ,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
