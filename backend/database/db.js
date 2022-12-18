839171694321fd826996288790f7ea9c9afb2ddc
Ishmam Solaiman <ishmam.a.solaiman@gmail.com>
2022-12-18 02:04:18 -0600

    fix db connection

--- backend/database/db.js
+++ backend/database/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async ()=>{
     try{
-        await mongoose.connect("mongodb+srv://ishmam:test123@cluster0.engz5.mongodb.net/?retryWrites=true&w=majority", {
+        await mongoose.connect("mongodb+srv://ishmam:test123@cluster0.engz5.mongodb.net/<dbname>?retryWrites=true&w=majority", {
             useNewUrlParser : true,
             useUnifiedTopology: true
         });
