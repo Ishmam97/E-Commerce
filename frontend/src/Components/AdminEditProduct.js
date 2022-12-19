3de1c9db705d909a68afa984dfba039207954b5b
Ishmam Solaiman <ishmam.a.solaiman@gmail.com>
2022-12-19 15:00:08 -0600

    remove navigate usage

--- frontend/src/Components/AdminEditProduct.js
+++ frontend/src/Components/AdminEditProduct.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState , useEffect } from "react";
-import { Link, Navigate, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import isEmpty from "validator/lib/isEmpty";
 
 import showMsg from "./helpers/Message";
