4c5f2a7c071d209294577b29b6dc3865661326fa
Ishmam Solaiman <ishmam.a.solaiman@gmail.com>
2022-12-21 00:56:46 -0600

    card fixup 2

--- frontend/src/Components/Card.js
+++ frontend/src/Components/Card.js
@@ -4,29 +4,30 @@ import "./css/card.css";
 
 import { useDispatch } from "react-redux";
 import { delProduct } from "../Redux/actions/productActions";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faEdit, faTrashAlt } from "@fortawesome/free-solid-svg-icons"
+
 function Card({ id, img, name, price, qt, cat, desc }) {
   const dispatch = useDispatch();
 
   return (
     <div className="card">
-      <h1 className="text-center">{name}</h1>
-      <div className="text-center">
-        <a href="#a">
-          <img
-            className="img-fluid pImg"
-            src={`http://localhost:9001/uploads/${img}`}
-            alt="product"
-          />
-        </a>
+      <h4 className="text-center text-truncate mt-2">{name}</h4>
+      <div className="image-container text-center">
+        <img
+          className="img-fluid pImg"
+          src={`http://localhost:9001/uploads/${img}`}
+          alt="product"
+        />
       </div>
-      <p className="text-justify text-center">
-        {desc.length > 100 ? desc.substring(0, 100) + "..." : desc}
+      <p className="text-truncate text-center">
+        {desc}
       </p>
-      <span className="text-center">Tk.{price}</span>
+      <span className="text-center">${price}</span>
       <div className="buttons">
         <Link to={`/admin/edit/product/${id}`}>
           <button className="btn btn-success btn-md m-1">
-            <i className="far fa-edit pr-1"></i>Edit
+            <FontAwesomeIcon icon={faEdit} className="pr-1" />
           </button>
         </Link>
 
@@ -36,7 +37,7 @@ function Card({ id, img, name, price, qt, cat, desc }) {
             dispatch(delProduct(id));
           }}
         >
-          <i className="far fa-trash-alt pr-1"></i>Delete
+          <FontAwesomeIcon icon={faTrashAlt} className="pr-1" />
         </button>
       </div>
     </div>
