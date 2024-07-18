145f0ed36f87d1f8d9aadfb156d63f987c66eeba
ishmam97 <ishmam.a.solaiman@gmail.com>
2024-07-18 03:10:58 -0500

    enhance product styling

--- frontend/src/Components/Product.js
+++ frontend/src/Components/Product.js
@@ -1,25 +1,25 @@
 import React from "react";
 import Item from "./Item";
 import { useSelector } from "react-redux";
+import "./css/products.css";
 
 const Product = () => {
   const { products } = useSelector((state) => state.products);
+
   return (
-    <div className="container d-flex gap-3 w-100">
-      {products.map((product, index) => {
-        return (
-          <Item
-            key={index}
-            id={product._id}
-            img={product.filename}
-            name={product.pName}
-            price={product.pPrice}
-            qt={product.pQty}
-            cat={product.pCat.category}
-            desc={product.pDesc}
-          />
-        );
-      })}
+    <div className="products-container">
+      {products.map((product, index) => (
+        <Item
+          key={index}
+          id={product._id}
+          img={product.filename}
+          name={product.pName}
+          price={product.pPrice}
+          qt={product.pQty}
+          cat={product.pCat.category}
+          desc={product.pDesc}
+        />
+      ))}
     </div>
   );
 };
