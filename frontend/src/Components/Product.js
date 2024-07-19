35a0079bb075b1e816f29e3ad89107bb45768de0
ishmam97 <ishmam.a.solaiman@gmail.com>
2024-07-19 01:48:55 -0500

    update products page and add filters

--- frontend/src/Components/Product.js
+++ frontend/src/Components/Product.js
@@ -1,25 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Item from "./Item";
+import Filter from "./Filters";
 import { useSelector } from "react-redux";
 import "./css/products.css";
 
 const Product = () => {
   const { products } = useSelector((state) => state.products);
+  const [filteredProducts, setFilteredProducts] = useState(products);
+
+  useEffect(() => {
+    setFilteredProducts(products);
+  }, [products]);
+
+  const handleFilterChange = (filters) => {
+    let filtered = products;
+
+    if (filters.category) {
+      filtered = filtered.filter(
+        (product) => product.pCat.category.toLowerCase() === filters.category.toLowerCase()
+      );
+    }
+
+    if (filters.priceRange) {
+      const [min, max] = filters.priceRange;
+      filtered = filtered.filter(
+        (product) => product.pPrice >= min && product.pPrice <= max
+      );
+    }
+
+    setFilteredProducts(filtered);
+  };
 
   return (
-    <div className="products-container">
-      {products.map((product, index) => (
-        <Item
-          key={index}
-          id={product._id}
-          img={product.filename}
-          name={product.pName}
-          price={product.pPrice}
-          qt={product.pQty}
-          cat={product.pCat.category}
-          desc={product.pDesc}
-        />
-      ))}
+    <div className="main-container">
+      <Filter onFilterChange={handleFilterChange} />
+      <div className="products-container">
+        {filteredProducts.map((product, index) => (
+          <Item
+            key={index}
+            id={product._id}
+            img={product.filename}
+            name={product.pName}
+            price={product.pPrice}
+            qt={product.pQty}
+            cat={product.pCat.category}
+            desc={product.pDesc}
+          />
+        ))}
+      </div>
     </div>
   );
 };
